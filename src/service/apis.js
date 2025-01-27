63b7332b77e5fb8a4e1eb9d4f54c02131611de73
TanyaSoni2912 <tanu139sona@gmail.com>
2025-01-27 11:58:04 +0530

    get all gps api and operation written

--- src/service/apis.js
+++ src/service/apis.js
@@ -8,3 +8,31 @@ export const authEndpoints = {
 	GET_USER: (username) =>
 		`${BASE}/api/UserProfile/GetUser?username=${username}`,
 };
+
+export const LocalPoiEndpoints = {
+	CREATE_LOCAL_POI: `${BASE}/api/LocalPOI/Create`,
+	UPDATE_LOCAL_POI: `${BASE}/api/LocalPOI/Update`,
+	DELETE_LOCAL_POI: `${BASE}/api/LocalPOI/Delete`,
+	UPLOAD_LOCAL_POI: `${BASE}/api/LocalPOI/Upload`,
+	GET_LOCAL_POI: `${BASE}/api/LocalPOI/GetPOI`,
+	GET_LOCAL_POI2: `${BASE}/api/LocalPOI/GetPOI2`,
+};
+
+export const availabilityEndpoints = {
+	GET_GENERAL_AVAILABILITY: `${BASE}/api/Availability/General`,
+	GET_REMINDER_AVAILABILITY: `${BASE}/api/Availability/Reminder`,
+};
+
+export const accountEndpoints = {
+	GETLIST_ACCOUNTS: `${BASE}/api/Accounts/GetList`,
+	UPLOAD_ACCOUNTS: `${BASE}/api/Accounts/Upload`,
+	GET_CLEAR_INVOICE: `${BASE}/api/Accounts/ClearInvoice`,
+};
+
+export const bookingsEndpoints = {
+	CANCEL_BOOKING: `${BASE}/api/Bookings/Cancel`,
+};
+
+export const gpsEndpoints = {
+	GET_ALL_GPS: `${BASE}/api/UserProfile/GetAllGPS`,
+};
---
+++ src/service/operations/gpsApi.js
@@ -0,0 +1,26 @@
+/** @format */
+
+import { sendLogs } from '../../utils/getLogs';
+import { handleGetReq, setHeaders } from '../apiRequestHandler';
+import { gpsEndpoints } from '../apis';
+
+const { GET_ALL_GPS } = gpsEndpoints;
+export async function gstAllGPS() {
+	// Fetch current user details using token
+	const response = await handleGetReq(GET_ALL_GPS);
+
+	console.log('GET ME API RESPONSE.........', response);
+
+	if (response.status === 'success') {
+		sendLogs(
+			{
+				url: GET_ALL_GPS,
+				reqBody: null,
+				headers: setHeaders(),
+				response: response,
+			},
+			'info'
+		);
+		return response;
+	}
+}
