1cbdc104cbf92effef02309d3d6d369fa6f22346
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-01-29 10:30:43 +0000

    UPDATED SEARCH BOOKING PAGE

--- src/pages/booking/searchBooking/addAccounts/index.js
+++ src/pages/booking/searchBooking/addAccounts/index.js
@@ -1,5 +1,3 @@
 /** @format */
 
-export * from './listAccounts';
 export * from './addAccounts';
-
--- src/pages/booking/searchBooking/searchBooking.jsx
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-function SearchBooking() {
-	return <div className='text-center text-gray-500'>Search Booking</div>;
-}
-
-export { SearchBooking };
