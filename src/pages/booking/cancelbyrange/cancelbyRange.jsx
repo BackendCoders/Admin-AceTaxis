ee5328829da9a0d08f3ac1dd33dda4a1663ab589
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-01-30 12:32:17 +0000

    updated cancel button here

--- src/pages/booking/cancelbyrange/cancelbyRange.jsx
+++ src/pages/booking/cancelbyrange/cancelbyRange.jsx
@@ -90,7 +90,7 @@ const CancelByRange = () => {
 						<div className='flex justify-end'>
 							<button
 								type='submit'
-								className='bg-green-700 hover:bg-green-900 text-white font-semibold px-6 py-2 rounded-md shadow-md transition-all'
+								className='bg-green-700 hover:bg-green-900 text-sm text-white font-semibold px-6 py-2 rounded-md shadow-md transition-all'
 							>
 								CANCEL JOBS
 							</button>
