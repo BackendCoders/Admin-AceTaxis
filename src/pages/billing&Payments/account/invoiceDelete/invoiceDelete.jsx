e8727898249039e60379a6632f79ce0d88a2dbbe
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-01-31 09:33:21 +0000

    updated invoice delete page

--- src/pages/billing&Payments/account/invoiceDelete/invoiceDelete.jsx
+++ src/pages/billing&Payments/account/invoiceDelete/invoiceDelete.jsx
@@ -12,33 +12,32 @@ import { format } from 'date-fns';
 import { cn } from '@/lib/utils';
 import { KeenIcon } from '@/components';
 
-const InvoiceDelete= () => {
-	
-
+const InvoiceDelete = () => {
 	return (
 		<div className='px-6 py-4 ms-auto me-auto max-w-[1580px] w-full'>
 			{/* Header Section */}
 			<h2 className='text-2xl font-semibold text-gray-900 dark:text-gray-700'>
-			Delete Invoice
+				Delete Invoice
 			</h2>
 
 			{/* Filter Inputs */}
 			<div className='flex flex-wrap items-center gap-6 mt-4'>
-
-			<div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
-          <label className="form-label max-w-56">Invoice Number</label>
-          <input type="text" className="input" placeholder="Invoice Number" />
-        </div>
-
-				{/* Date Range Picker */}
-				<div className='flex flex-col'>
-					
+				<div className='flex items-baseline flex-wrap lg:flex-nowrap gap-2.5'>
+					{/* <label className='form-label max-w-56'>Invoice Number</label> */}
+					<input
+						type='text'
+						className='input'
+						placeholder='Invoice Number'
+					/>
 				</div>
 
-				<button className="btn btn-primary flex justify-center">CONTINUE</button>
+
+				<button className='btn btn-primary flex justify-center'>
+					CONTINUE
+				</button>
 			</div>
 		</div>
 	);
 };
 
-export { InvoiceDelete};
+export { InvoiceDelete };
