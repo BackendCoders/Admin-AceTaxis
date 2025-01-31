9d9d7f9de1e66e220ecbe1be35963775b6c7487b
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-01-31 09:23:24 +0000

    updated invoice delete page here

--- src/pages/billing&Payments/account/invoiceDelete/invoiceDelete.jsx
+++ src/pages/billing&Payments/account/invoiceDelete/invoiceDelete.jsx
@@ -1,7 +1,44 @@
 /** @format */
 
-function InvoiceDelete() {
-	return <div className='text-center text-gray-500'>Invoice Delete</div>;
-}
+import { useState } from 'react';
+import { IoChevronUpSharp, IoChevronDownSharp } from 'react-icons/io5';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
+import { format } from 'date-fns';
+import { cn } from '@/lib/utils';
+import { KeenIcon } from '@/components';
 
-export { InvoiceDelete };
+const InvoiceDelete= () => {
+	
+
+	return (
+		<div className='px-6 py-4 ms-auto me-auto max-w-[1580px] w-full'>
+			{/* Header Section */}
+			<h2 className='text-2xl font-semibold text-gray-900 dark:text-gray-700'>
+			Delete Invoice
+			</h2>
+
+			{/* Filter Inputs */}
+			<div className='flex flex-wrap items-center gap-6 mt-4'>
+
+			<div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Invoice Number</label>
+          <input type="text" className="input" placeholder="Invoice Number" />
+        </div>
+
+				{/* Date Range Picker */}
+				<div className='flex flex-col'>
+					
+				</div>
+
+				<button className="btn btn-primary flex justify-center">CONTINUE</button>
+			</div>
+		</div>
+	);
+};
+
+export { InvoiceDelete};
