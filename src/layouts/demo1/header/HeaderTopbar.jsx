a5da3ddc864e0020c44f4324229dc5fad04b77fb
TanyaSoni2912 <tanu139sona@gmail.com>
2025-04-01 15:59:19 +0530

    Heart Beat Color change And Refresh dashboard after 60 sec

--- src/layouts/demo1/header/HeaderTopbar.jsx
+++ src/layouts/demo1/header/HeaderTopbar.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { KeenIcon } from '@/components/keenicons';
 import { toAbsoluteUrl } from '@/utils';
 import { Menu, MenuItem, MenuToggle } from '@/components';
@@ -17,6 +17,7 @@ const HeaderTopbar = () => {
 	const dispatch = useDispatch();
 	const { unreadCount } = useSelector((state) => state.notification);
 	const { smsHeartBeat } = useSelector((state) => state.dashboard);
+	const [smsHeartBeatColor, setSmsHeartBeatColor] = useState();
 	const { isRTL } = useLanguage();
 	const itemChatRef = useRef(null);
 	const itemAppsRef = useRef(null);
@@ -32,8 +33,30 @@ const HeaderTopbar = () => {
 	// };
 
 	useEffect(() => {
-		dispatch(refreshDashboard());
+		const interval = setInterval(() => {
+			dispatch(refreshDashboard());
+		}, 60000); // 1 minute interval
+		return () => clearInterval(interval); // Cleanup on unmount
 	}, [dispatch]);
+
+	useEffect(() => {
+		const checkHeartBeat = () => {
+			if (!smsHeartBeat) return;
+
+			const now = new Date();
+			const heartBeatTime = new Date(smsHeartBeat);
+			const diffInSeconds = (now - heartBeatTime) / 1000;
+
+			if (diffInSeconds > 120 || diffInSeconds < -120) {
+				setSmsHeartBeatColor({ light: 'bg-danger', dark: 'bg-danger' });
+			} else {
+				setSmsHeartBeatColor({ light: 'bg-green-400', dark: 'bg-green-700' });
+			}
+		};
+
+		checkHeartBeat();
+	}, [smsHeartBeat]);
+
 	return (
 		<div className='flex items-center gap-2 lg:gap-3.5'>
 			{/* <button onClick={handleOpen} className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary text-gray-500">
@@ -94,7 +117,9 @@ const HeaderTopbar = () => {
 				</MenuItem>
 			</Menu>
 
-			<div className='flex justify-center items-center bg-green-400 dark:bg-green-700 text-2xs sm:text-sm text-white px-2 sm:px-3 py-1 sm:py-2 rounded-md'>
+			<div
+				className={`flex justify-center items-center ${smsHeartBeatColor?.light} dark:${smsHeartBeatColor?.dark} text-2xs sm:text-sm text-white px-2 sm:px-3 py-1 sm:py-2 rounded-md`}
+			>
 				SMS HEARTBEAT{' '}
 				{new Date(smsHeartBeat?.split('T')[0]).toLocaleDateString('en-GB')}{' '}
 				{smsHeartBeat?.split('T')[1].split('.')[0]}
